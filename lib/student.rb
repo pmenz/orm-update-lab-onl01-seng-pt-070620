489f69dd6abe0edd32e8b0f32dbcd221c93171de
Pablo Menz <p.menz.r@gmail.com>
2020-10-08 18:25:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,21 +3,16 @@ require_relative "../config/environment.rb"
 class Student
 
   attr_accessor :name, :grade
-<<<<<<< HEAD
   attr_reader :id
 
   def initialize(id=nil,name, grade)
     @id= id
-=======
-
-  def initialize(name, grade)
->>>>>>> 67c9b297cd0f27167b76752215b281847198d8e5
     @name= name
     @grade= grade
   end
 
   def self.create_table
-<<<<<<< HEAD
+
     sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
@@ -46,64 +41,37 @@ class Student
 
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
+    end 
 
-    end
-=======
-  end
-
-  def self.drop_table
-  end
->>>>>>> 67c9b297cd0f27167b76752215b281847198d8e5
-
-  def self.create
-  end
-
-  def self.new_from_db
-  end
-
-  def self.find_by_name
-  end
-
-  def update
-  end
-    # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-  end
 
   def self.create(name, grade)
       student = Student.new(name, grade)
       student.save
-    end
+  end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+      self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-  SQL
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+      SQL
 
-  DB[:conn].execute(sql,name).map do |row|
-    self.new_from_db(row)
-  end.first
+      DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+      end.first
   end
 
   def update
-   sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-   DB[:conn].execute(sql, self.name, self.grade, self.id)
+     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
  end
-    # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
 end
